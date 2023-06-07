a44810b6f6620a6690997f41d92b8c747dc6f5fe
jaomarcs7 <jaomarcs7@gmail.com>
2023-06-06 21:35:55 -0300

    Adicionando primeiros elementos da pagina, Icone e Texto de entrada com formatacao

--- app/src/main/java/com/example/aula1/MainActivity.kt
+++ app/src/main/java/com/example/aula1/MainActivity.kt
@@ -3,21 +3,15 @@ package com.example.aula1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
-import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.node.modifierElementOf
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.aula1.ui.theme.Aula1Theme
 
